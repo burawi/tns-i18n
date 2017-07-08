7add60b2df2813991d73b0f6998bee3a83b771e4
burawi <burawi.ba@gmail.com>
2017-07-08 21:56:32 +0100

    switch to application.setResources()

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ var strRender = require('str-render');
 
 module.exports = function (defaultLang) {
 
-    var lang = platformModule.device.language;
-    var defaults = require('~/i18n/'+defaultLang);
-    var strings = {};
-    try {
-        strings = require('~/i18n/'+lang);
-    } catch (e) { }
+  var lang = platformModule.device.language;
+  var defaults = require('~/i18n/' + defaultLang);
+  var strings = {};
+  try {
+    strings = require('~/i18n/' + lang);
+  } catch (e) { }
 
 
-    var _L = function (strName, ...replacers) {
-        var res = '';
-        if(strings.hasOwnProperty(strName))
-            res = strings[strName];
-        else if(defaults.hasOwnProperty(strName))
-            res = defaults[strName];
-        return strRender(res, '%s', ...replacers);
-    }
+  var _L = function (strName, ...replacers) {
+    var res = '';
+    if (strings.hasOwnProperty(strName))
+      res = strings[strName];
+    else if (defaults.hasOwnProperty(strName))
+      res = defaults[strName];
+    return strRender(res, '%s', ...replacers);
+  }
 
-    application.resources._L = _L;
-    global._L = _L;
+  application.setResources(_L);
+  global._L = _L;
 };
